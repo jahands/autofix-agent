f56d929c38b5bea57f3224ab95ff4e533dce9a7e
Jacob Hands <jacob@jacobhands.com>
2025-05-14 16:43:06 -0500

    saffold API

--- apps/autofix-agent/src/autofix-agent.app.ts
+++ apps/autofix-agent/src/autofix-agent.app.ts
@@ -1,6 +1,12 @@
+import { sValidator } from '@hono/standard-validator'
 import { Agent } from 'agents'
+import { Hono } from 'hono'
+import { useWorkersLogger } from 'workers-tagged-logger'
+import { z } from 'zod'
 
-import type { Env } from './autofix-agent.context'
+import { useNotFound, useOnError } from '@repo/hono-helpers'
+
+import type { App, Env } from './autofix-agent.context'
 
 type State = {
 	repo: string
@@ -16,4 +22,33 @@ export class AutofixAgent extends Agent<Env, State> {
 	// can run for seconds, minutes or hours: as long as the tasks need.
 }
 
-export default AutofixAgent
+const app = new Hono<App>()
+	.use(
+		'*',
+		// middleware
+		(c, next) =>
+			useWorkersLogger(c.env.NAME, {
+				environment: c.env.ENVIRONMENT,
+				release: c.env.SENTRY_RELEASE,
+			})(c, next)
+	)
+
+	.onError(useOnError())
+	.notFound(useNotFound())
+
+	.get(
+		'/api/agents/:agentId',
+		sValidator(
+			'query',
+			z.object({
+				agentId: z.string(),
+			})
+		),
+		async (c) => {
+			const { agentId } = c.req.valid('query')
+			const id = c.env.AutofixAgent.idFromName(agentId)
+			const agent = c.env.AutofixAgent.get(id)
+		}
+	)
+
+export default app
--- apps/autofix-agent/src/autofix-agent.context.ts
+++ apps/autofix-agent/src/autofix-agent.context.ts
@@ -1,8 +1,11 @@
 import type { HonoApp } from '@repo/hono-helpers'
 import type { SharedHonoEnv, SharedHonoVariables } from '@repo/hono-helpers/src/types'
+import type { AutofixAgent } from './autofix-agent.app'
 
 export type Env = SharedHonoEnv & {
 	// add additional Bindings here
+
+	AutofixAgent: DurableObjectNamespace<AutofixAgent>
 }
 
 /** Variables can be extended */
