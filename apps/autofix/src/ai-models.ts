999022a6d5e0bedf4ec5de3a72f45896e3599d58
Josh Wheeler <jwheeler@cloudflare.com>
2025-05-19 15:45:04 -0400

    feature: add AI gateway for 3rd party models

--- apps/autofix/src/ai-models.ts
+++ apps/autofix/src/ai-models.ts
@@ -1,10 +1,26 @@
+import { createAnthropic } from '@ai-sdk/anthropic'
+import { createOpenAI } from '@ai-sdk/openai'
+import { createAiGateway } from 'ai-gateway-provider'
 import { env } from 'cloudflare:workers'
 import { createWorkersAI } from 'workers-ai-provider'
 
-// We can add other models or providers (openAI) here
+const workersAi = createWorkersAI({ binding: env.AI })
+export const WorkersAIModels = {
+	llama4: () => workersAi('@cf/meta/llama-4-scout-17b-16e-instruct' as any, {}),
+}
 
-const workersAiInstance = createWorkersAI({ binding: env.AI })
+const aiGateway = createAiGateway({
+	accountId: env.AI_GATEWAY_ACCOUNT_ID,
+	gateway: env.AI_GATEWAY_NAME,
+	apiKey: env.AI_GATEWAY_API_KEY,
+})
 
-export const WorkersAiModels = {
-	Llama4: workersAiInstance('@cf/meta/llama-4-scout-17b-16e-instruct' as any, {}),
+const anthropic = createAnthropic({ apiKey: '' })
+export const AnthropicModels = {
+	claude: () => aiGateway([anthropic('claude-3-7-sonnet-20250219')]),
+}
+
+const openAi = createOpenAI({ apiKey: '' })
+export const OpenAIModels = {
+	GPT4o: () => aiGateway([openAi('chatgpt-4o-latest')]),
 }
